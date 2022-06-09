bf97e04cbd6fb8d34ae0379c4904f0f856ff1d21
Lucas Oliveira Jacintho <lucas.jacintho@iteris.com.br>
2022-06-09 14:16:11 -0300

    Importing Material Table, and creating a course table

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
