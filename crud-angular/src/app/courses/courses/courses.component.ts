0452f7680cccff7fcd8ecbca9fb8995602ced067
Lucas Oliveira Jacintho <lucas.jacintho@iteris.com.br>
2022-06-10 15:06:07 -0300

    exception treatment, insert dialogues

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, of } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { Course } from './../model/course';
 import { CoursesService } from './../services/courses.service';
@@ -16,11 +19,16 @@ export class CoursesComponent implements OnInit {
 
   //coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
     //this.courses=[];
     //this.coursesService = new CoursesService();
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+    .pipe(catchError ( error => { this.onError('Erro ao carregar tabela de cursos.');return of([])}));
     //this.coursesService.list().subscribe(courses => this.courses = courses);
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {data: errorMsg});
   }
 
   ngOnInit(): void {
