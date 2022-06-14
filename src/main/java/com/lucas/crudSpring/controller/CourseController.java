4deeab925e47a36616b0653afcd269370c3748d0
Lucas Oliveira Jacintho <lucas.jacintho@iteris.com.br>
2022-06-14 13:14:16 -0300

    Solving conection problem between Angular and Spring

--- src/main/java/com/lucas/crudSpring/controller/CourseController.java
+++ src/main/java/com/lucas/crudSpring/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.lucas.crudSpring.controller;
 import com.lucas.crudSpring.model.Course;
 import com.lucas.crudSpring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
+@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 public class CourseController {
 
--- src/main/java/com/lucas/crudSpring/model/Course.java
+++ src/main/java/com/lucas/crudSpring/model/Course.java
@@ -1,5 +1,6 @@
 package com.lucas.crudSpring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -12,6 +13,7 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
+    @JsonProperty("_id")
     private Long id;
 
     @Column (length = 200, nullable = false)
